3d3212f5cddd0432180e58e8e7836bf294df4243
Ernesto Zeferino Diaz <ernesto.zefe.d@gmail.com>
2020-10-03 22:18:54 -0500

    First Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char items[] = {'a','b','c','d','e','f','g','h','i'};
+    int n,r;
+
+    cout<<"\n N:";
+    cin>>n;
+    cout<<"\n R:";
+    cin>>r;
+
+    return 0;
+}
