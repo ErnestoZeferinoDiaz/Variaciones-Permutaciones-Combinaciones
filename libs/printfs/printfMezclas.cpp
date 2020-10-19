79ac70e0afb2d1c14aeb1c1689640fc6b09412ec
Ernesto Zeferino Diaz <ernesto.zefe.d@gmail.com>
2020-10-18 20:41:59 -0500

    Optimizacion de printfs mezclas

--- libs/printfs/printfMezclas.cpp
+++ libs/printfs/printfMezclas.cpp
@@ -1,59 +1,55 @@
-void printfVariacionCR(char* items,int n,int r){
+void plantilla(char* items,int n,int r,int size,const char* title,void(*typeMezcla)(char*,char**,int,int,int*,int,int*)){
     char** matriz;
-    int size;
-    size = pow(n,r);
     matriz = initMatriz(size,r);
-    variacionCR(items,matriz,n,r,NULL,0,NULL);
-    printf("\n\n *********** Variacion con repeticion ************* ");
+    typeMezcla(items,matriz,n,r,NULL,0,NULL);
+    printf("\n\n *********** %s ************* ",title);
     printf("\n Total de mezclas: %d",size);
     printf("\n Lista de mezclas:");
     printMatriz(matriz,size,r);
+}
+
+
+void printfVariacionCR(char* items,int n,int r){
+    plantilla(
+        items,n,r,
+        pow(n,r),
+        "Variacion con repticion",
+        variacionCR
+    );
 }
 
 void printfVariacionSR(char* items,int n,int r){
-    char** matriz;
-    int size;
-    size = fact(n)/fact(n-r);
-    matriz = initMatriz(size,r);
-    variacionSR(items,matriz,n,r,NULL,0,NULL);
-    printf("\n\n *********** Variacion sin repeticion ************* ");
-    printf("\n Total de mezclas: %d",size);
-    printf("\n Lista de mezclas:");
-    printMatriz(matriz,size,r);
+    plantilla(
+        items,n,r,
+        fact(n)/fact(n-r),
+        "Variacion sin repticion",
+        variacionSR
+    );
 }
 
 void printfPermutacionCirc(char* items,int n,int r){
-    char** matriz;
-    int size;
-    size = fact(n-1);
-    matriz = initMatriz(size,n);
-    permutacionCirc(items,matriz,n,n,NULL,0,NULL);
-    printf("\n\n *********** Permutacion circular ************* ");
-    printf("\n Total de mezclas: %d",size);
-    printf("\n Lista de mezclas:");
-    printMatriz(matriz,size,n);
+    plantilla(
+        items,n,n,
+        fact(n-1),
+        "Permutacion circular",
+        permutacionCirc
+    );
 }
 
 void printfCombinacionCR(char* items,int n,int r){
-    char** matriz;
-    int size;
-    size = fact(n+r-1)/fact(r)/fact(n-1);
-    matriz = initMatriz(size,r);
-    combinacionCR(items,matriz,n,r,NULL,0,NULL);
-    printf("\n\n *********** Combinacion con repeticion ************* ");
-    printf("\n Total de mezclas: %d",size);
-    printf("\n Lista de mezclas:");
-    printMatriz(matriz,size,r);
+    plantilla(
+        items,n,r,
+        fact(n+r-1)/fact(r)/fact(n-1),
+        "Combinacion con repeticion",
+        combinacionCR
+    );
 }
 
 void printfCombinacionSR(char* items,int n,int r){
-    char** matriz;
-    int size;
-    size = fact(n)/fact(r)/fact(n-r);
-    matriz = initMatriz(size,r);
-    combinacionSR(items,matriz,n,r,NULL,0,NULL);
-    printf("\n\n *********** Combinacion sin repeticion ************* ");
-    printf("\n Total de mezclas: %d",size);
-    printf("\n Lista de mezclas:");
-    printMatriz(matriz,size,r);
+    plantilla(
+        items,n,r,
+        fact(n)/fact(r)/fact(n-r),
+        "Combinacion sin repeticion",
+        combinacionSR
+    );
 }
